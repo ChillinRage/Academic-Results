4fb4c54e0ecc69d95314758d399415ecdb055f58
Chan How <73763719+ChillinRage@users.noreply.github.com>
2024-10-16 23:27:29 +0800

    Improve stats for summary report

--- src/react-components/tsx/Summary.tsx
+++ src/react-components/tsx/Summary.tsx
@@ -7,7 +7,9 @@ import '../css/Summary.css';
 interface Report {
     date: string,
     units: number,
+    unitDiff: number,
     score: string,
+    scoreDiff: string,
 }
 
 interface Props {
@@ -24,12 +26,18 @@ const Summary = ({moduleList, setModal} : Props) => {
     <h2 className='report-header'>Academic Year & Semester | Cumulative Units | Cumulative GPA</h2>
 
     {generateReport(moduleList).map(report =>
-      <h3 className='report-row'>{report.date} | {report.units} | {report.score}</h3>)}
+      <h3 className='report-row'>
+        {report.date}  &nbsp;|
+        &nbsp; {report.units} (+{report.unitDiff})  &nbsp;|
+        &nbsp; {report.score} ({report.scoreDiff})
+      </h3>)}
   </div>
 }
 
 function generateReport(moduleList: Module[]): Report[] {
   const reports: Report[] = [];
+  let date, units, score, unitDiff, scoreDiff, filteredList;
+  let prevUnits = 0, prevScore = 0.00;
   let filterFunc = (module: Module) => false;
 
   for (let year = 1; year < 5; year++) {
@@ -38,13 +46,18 @@ function generateReport(moduleList: Module[]): Report[] {
       if (moduleList.filter(newFilter).length === 0) break;
 
       filterFunc = orFilter(newFilter, filterFunc);
-      const filteredList = moduleList.filter(filterFunc);
+      filteredList = moduleList.filter(filterFunc);
 
-      const date = `AY${21+year}/${22+year} S${semester}`;
-      const units = filteredList.reduce((acc, current) => acc + current.unit, 0);
-      const score = calculateScore(filteredList, true);
+      date = `AY${21+year}/${22+year} S${semester}`;
+      units = filteredList.reduce((acc, current) => acc + current.unit, 0);
+      score = calculateScore(filteredList, true);
+      unitDiff = units - prevUnits;
+      scoreDiff = parseFloat(score) - prevScore;
+      scoreDiff = (scoreDiff >= 0 ? '+' : '') + scoreDiff.toFixed(2);
 
-      reports.push({date, units, score});
+      reports.push({date, units, unitDiff, score, scoreDiff});
+      prevUnits = units;
+      prevScore = parseFloat(score);
     }
   }
   return reports;
