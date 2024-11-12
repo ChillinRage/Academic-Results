611034a5e0f21f7224a96edbbde196f4e570c38d
Chan How <73763719+ChillinRage@users.noreply.github.com>
2024-11-12 23:06:43 +0800

    fix screen resolution

---
+++ src/scripts/Firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import firebaseConfig from "../assets/firebaseConfig.json";
+
+async function getURL(): Promise<string> {
+  // Initialize and authenticate Firebase
+  if (location.hostname === 'localhost') // for local development
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.FIREBASE_APPCHECK_DEBUG_TOKEN;
+
+  const app = initializeApp(firebaseConfig);
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(process.env.CAPTCHA_SITE_KEY!),
+    isTokenAutoRefreshEnabled: true
+  });
+
+  const storage = getStorage(app);
+  const resultsRef = ref(storage, "results.csv");
+  const url = await getDownloadURL(resultsRef)
+  return url;
+}
+
+export {getURL};
